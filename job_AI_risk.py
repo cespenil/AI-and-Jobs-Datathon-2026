72df129e19825a68072db8faf39b2cc9767dcaad
cespenil <cnicole0131@gmail.com>
2026-04-05 03:09:37 -0700

    Update job_AI_risk.py
    Finalized version that is fully compatible with Omni to visualize careers' relation to demand and AI risk.

--- job_AI_risk.py
+++ job_AI_risk.py
@@ -29,22 +29,23 @@ def risk_category(risk):
 job_summary['risk_category'] = job_summary['ai_risk_score'].apply(risk_category)
 
 
-# Create demand category for quadrants
-job_summary['demand_category'] = job_summary['skill_demand_score'].apply(
-    lambda x: 'High Demand' if x >= 80 else 'Low Demand'
-)
-
-
-# Combine risk + demand into quadrants
-job_summary['quadrant'] = job_summary['risk_category'] + " / " + job_summary['demand_category']
-
-
 # Risk-adjusted job openings (numeric feature)
 # Basically a feature that calculates the amount of "safe" jobs based on the
 # openings and AI risk score
 job_summary['risk_adjusted_openings'] = job_summary['job_openings'] * (1 - job_summary['ai_risk_score'])
 # Scale risk-adjusted openings for easier display (In the millions)
 job_summary['risk_adjusted_openings_millions'] = job_summary['risk_adjusted_openings'] / 1e6
+
+
+# Create demand category for quadrants
+median_safe_jobs = job_summary['risk_adjusted_openings_millions'].median()
+job_summary['demand_category'] = job_summary['risk_adjusted_openings_millions'].apply(
+    lambda x: 'High Demand' if x >= median_safe_jobs else 'Low Demand'
+)
+
+
+# Combine risk + demand into quadrants
+job_summary['quadrant'] = job_summary['risk_category'] + " / " + job_summary['demand_category']
 
 
 # Ranking by safety
@@ -57,6 +58,10 @@ job_summary['safety_score_norm'] = (
 ) / (
     job_summary['risk_adjusted_openings'].max() - job_summary['risk_adjusted_openings'].min()
 )
+
+
+# Creating a csv file to use in Omni
+job_summary.to_csv("job_summary_featured.csv", index=False)
 
 # Checking dataframe
 print(job_summary.head())
