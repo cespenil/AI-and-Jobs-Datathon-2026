ae1a6c4d7c64c9eed6398a5292fd61ade66482e5
varsshir <varsshir@uci.edu>
2026-04-04 22:50:49 -0700

    Update year_vs_salary_ml.py
    i added the visualization of the linear regression

--- year_vs_salary_ml.py
+++ year_vs_salary_ml.py
@@ -6,9 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from sklearn.linear_model import LinearRegression
 
-import pandas as pd
-import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
 
 # X data and Y data
@@ -40,3 +37,34 @@ print(r2_score(y_train, y_train_pred))
 
 print("The r2 score of the cleaned salary testing data:")
 print(r2_score(y_test, y_test_pred))
+
+# creating csv for visulaization
+df_FE.to_csv("year_salary_processed.csv", index=False)
+
+
+# visualization
+df_plot = df_FE.sort_values("year")
+predicted_salary_line = model.predict(df_plot[["year_from_start"]])
+
+plt.figure(figsize=(10,6))
+# scatter plot
+plt.scatter(df_plot["year"],
+            df_plot["salary_cleaned"],
+            alpha=0.3,
+            label="Actual data"
+            )
+#linear regression line
+plt.plot(df_plot["year"],
+         predicted_salary_line,
+         color = "red",
+         linewidth=2,
+         label = "Regression Line"
+         )
+#labels and titles
+plt.title("Linear Regression: Year vs Salary")
+plt.xlabel("Year")
+plt.ylabel("Salary Cleaned (USD)")
+plt.legend()
+plt.grid(True)
+
+plt.show()
